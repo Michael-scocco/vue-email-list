4f2bd69a6478d7eee3e03fb31508f04a5f9dec34
Michael Scocco <scocco.michael87@gmail.com>
2021-05-27 16:42:54 +0200

    vai a capi l'errore

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/*
+Esercizio di oggi, Vue Email List nome repo:
+vue-email-list
+Descrizione:
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus:
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+Le slide sono su drive! a breve anche il coding di oggi
+*/
+const app = new Vue({
+    el: '#app',
+    data: {
+        listaEmail:[],
+    },
+    methods:{
+
+    },
+    mounted(){
+        var i =0;
+        while(i < 10){
+        axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")//questo ci promette di darci una risposta;
+        .then(response => {
+            console.log(response.data.response);
+            this.listaEmail.push(this.response.data.response);
+        })
+        i++;
+        };
+        
+    },
+
+})
