8d0c23a0de91151657ea46c2e12f556ef8fc0f37
Michael Scocco <scocco.michael87@gmail.com>
2021-06-07 09:48:42 +0200

    finito

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ Bonus:
 Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 Le slide sono su drive! a breve anche il coding di oggi
 */
+
 const app = new Vue({
     el: '#app',
     data: {
@@ -37,62 +38,28 @@ const app = new Vue({
 const app = new Vue({
     el: '#app',
     data: {
-        counter: 0,
-        images: [
-            'https://scitechdaily.com/images/Mars-Red-Planet-Highlights-scaled.jpg',
-            'https://media.newyorker.com/photos/60994dabccd28612d3013cec/master/pass/Mann-SpaceEthics.jpg',
-            'https://cdn.britannica.com/24/73524-050-3C2F3432/result-seasons-Mars-planet-inclination-plane-cap.jpg',
-            'https://cdn.mos.cms.futurecdn.net/iWG5zg5Mpt8AvZqc4vYa4K.jpg'
-        ],
-        text:'Hello',
-        textHTML: '<h1>Hello</h1>',
-        disButton: true,
-        number: 10,
-        ok: true
-       
+        url: 'https://flynn.boolean.careers/exercises//random/mail',
+        emails: []
+    },
+    methods: {
 
     },
-    methods:{
-        prev(){
-            // do something
-            console.log('Cliccato su prev');
-            if(this.counter === 0){   
-                return this.counter = this.images.length -1
-            }
-            return this.counter -= 1
+    mounted() {
 
-        },
-        next(){
-            // do something
-            console.log('Cliccato su next');
-            
-            if(this.counter === this.images.length -1){
-               
-                return this.counter = 0
-            }
-            return this.counter += 1
 
-        },
+        for (let index = 0; index < 10; index++) {
 
-        selectImage(index){
-            console.log(index);
-            return this.counter = index
+            axios
+                .get(this.url)
+                .then(resp => {
+                    //console.log(resp);
+                    this.emails.push(resp.data.response)
+                }).catch(e => {
+                    console.error(e);
+                })
+
+
         }
-
-    },
-    mounted(){
-        // Add event listener for keys events
-      document.addEventListener('keyup', (e) => {  
-        if(e.key === 'ArrowRight'){  
-            this.next();
-        }
-        if(e.key === 'ArrowLeft'){
-            this.prev();
-        }
-      })
-
-        // Add infinite sliding
-
-      setInterval(this.next, 5000)
     }
-})
+
+});
